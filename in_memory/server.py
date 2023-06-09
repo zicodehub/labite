bb0bc8c118cf6103321aa19f267de355cfd7dd42
marcleord <marcleord@gmail.com>
2023-06-09 05:23:56 +0000

    ✨ Sparkle server for Recuit

---
+++ in_memory/server.py
@@ -0,0 +1,53 @@
+import random
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from _types import *
+from utils import reset_all
+from model_warehouse import *
+from model_vehicule import *
+from model_compartment import *
+from model_type_article import *
+from model_article import *
+from model_client import *
+from model_supplier import *
+from model_order import *
+
+from recuit import RecuitSimule
+
+class APIInput(BaseModel):
+    clients: Any
+    suppliers: List
+    type_articles: List
+    articles: List
+    orders: List
+    warehouses: List
+    vehicules: List
+
+app = FastAPI()
+
+@app.post("/recuit")
+async def recuit(obj: APIInput):
+    print(obj)
+    reset_all()
+
+    ClientModel.create_many(obj.clients)
+    SupplierModel.create_many(obj.suppliers)
+    TypeArticleModel.create_many(obj.type_articles)
+    ArticleModel.create_many(obj.articles)
+    OrderModel.create_many(obj.orders)
+    WarehoudeModel.create_many(obj.warehouses)
+    VehiculeModel.create_many(obj.vehicules)
+
+    f = OrderModel.get_fournisseurs()
+    c = OrderModel.get_clients()
+    d = WarehoudeModel.get(1)
+    print(f"Il y a {len(f)} fourn et {len(c)} clients ")
+
+    random.shuffle(f)
+    random.shuffle(c)
+
+    # list_initial = [d] + f + c + [d]
+    list_initial = f + c 
+    recuit = RecuitSimule(list_initial)
+    res = recuit.start()
+    return res
